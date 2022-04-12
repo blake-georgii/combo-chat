6a8b1adc6de110bad76d40671aef6ff76677eec2
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2022-04-11 19:16:48 -0700

    checking branch

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -62,7 +62,7 @@ export const ADD_BOOK = gql`
 
 
 export const REMOVE_BOOK = gql`
-mutation removeBook($bookId: String!) {
+mutation removeBook($bookId: String} {
   removeBook(bookId: $bookId) {
     _id
     username
