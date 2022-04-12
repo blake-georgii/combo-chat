db2e528bbedf21e5c9f404c9f16de9a3b7943bdc
Matt Foster <fmatthew40@gmail.com>
2022-04-11 19:19:53 -0700

    update to variables

--- client/src/pages/SearchGames.js
+++ client/src/pages/SearchGames.js
@@ -80,6 +80,7 @@ const SearchGames = () => {
   return (
     <>
       <Jumbotron fluid className='text-light bg-dark'>
+        
         <Container>
           <h1>Search for Games!</h1>
           <Form onSubmit={handleFormSubmit}>
